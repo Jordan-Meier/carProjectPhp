75ae254f220588b231dbbd072fd3d1b390032563
Jordan Meier and Ryan Brown <browneryan87@gmail.com>
2016-02-08 09:38:45 -0800

    Add a constructor to Car class and replace many lines of code with 4 lines of refactored code

--- car.php
+++ car.php
@@ -5,31 +5,19 @@
         public $price;
         public $miles;
 
+        function __construct($car_model, $car_price, $car_miles)
+        {
+            $this->make_model = $car_model;
+            $this->price = $car_price;
+            $this->miles = $car_miles;
+        }
+
         function worthBuying($max_price)
         {
             return $this->price <= ($max_price + 500);
         }
     }
 
-    $honda = new Car();
-    $honda->make_model = "1999 Honda CRV";
-    $honda->price = 6000;
-    $honda->miles = 90000;
-
-    $tesla = new Car();
-    $tesla->make_model =  "2014 Tesla Model S";
-    $tesla->price = 35000;
-    $tesla->miles = 5000;
-
-    $nissan = new Car();
-    $nissan->make_model = "2013 Nissan Leaf";
-    $nissan->price = 20000;
-    $nissan->miles = 8000;
-
-    $toyota = new Car();
-    $toyota->make_model = "2009 Toyota Prius";
-    $toyota->price = 15000;
-    $toyota->miles = 20000;
 
     $cars = array($honda, $tesla, $nissan, $toyota);
 
